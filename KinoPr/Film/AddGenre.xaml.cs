3d36b8a0b52da45a3423a6ad249685c21ed11cd8
timofeevsasha22@gmail.com <timofeevsasha22@gmail.com>
2024-04-22 17:31:13 +0700

    da

---
+++ KinoPr/Film/AddGenre.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace KinoPr
+{
+    /// <summary>
+    /// Логика взаимодействия для AddGenre.xaml
+    /// </summary>
+    public partial class AddGenre : Page
+    {
+        public AddGenre()
+        {
+            InitializeComponent();
+        }
+
+        private void BackButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
