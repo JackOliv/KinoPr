4a2d11f74eb685b7f0aa94dd1c1035d4cc998674
timofeevsasha22@gmail.com <timofeevsasha22@gmail.com>
2024-04-23 19:02:03 +0700

    da

--- KinoPr/Film/EditFilm.xaml.cs
+++ KinoPr/Film/EditFilm.xaml.cs
@@ -31,6 +31,10 @@ namespace KinoPr
         {
             FrameManager.MainFrame.Navigate(new AdminPage(mainWindow));
         }
+        private void ChangePhotoButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
         private void EditButton_Click(object sender, RoutedEventArgs e)
         {
 
