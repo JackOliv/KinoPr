ce5ff792a1d0bad5c381fc4a810c0f781116f5e3
timofeevsasha22@gmail.com <timofeevsasha22@gmail.com>
2024-04-23 20:06:26 +0700

    da

--- KinoPr/Models/Film.cs
+++ KinoPr/Models/Film.cs
@@ -8,8 +8,6 @@ namespace KinoPr
 {
     public class Film
     {
-        public class Film
-        {
             public int Id { get; set; }
             public string Name { get; set; }
             public int Duration { get; set; }
@@ -19,6 +17,6 @@ namespace KinoPr
             public string Description { get; set; }
             public string Director { get; set; }
             public string Country { get; set; }
-        }
+        
     }
 }
